2f674bc70cf0eeb2fba5f31caeb210c7ff5d89b6
austonj <auston.jin@gmail.com>
2017-04-16 20:46:59 -0400

    Fix incorrect daily balance amount

--- src/com/auston/TransactionCache.java
+++ src/com/auston/TransactionCache.java
@@ -1,23 +1,58 @@
 package com.auston;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 public class TransactionCache {
-	private static final int DATE_MAP_SIZE = 20;
+	private static final int CACHE_SIZE = 20;
 	private static TransactionCache transactionInfo = null;
-	private final Map<Date, Double> cache = new LinkedHashMap<Date, Double>(DATE_MAP_SIZE,
+
+	private final Map<Date, Double> cache = new LinkedHashMap<Date, Double>(CACHE_SIZE,
 			(float) 0.75, true) {
 		private static final long serialVersionUID = 1L;
 
 		@Override
-		protected boolean removeEldestEntry(Map.Entry<Date, Double> eldest) {
-			return size() > DATE_MAP_SIZE;
+		protected synchronized boolean removeEldestEntry(Map.Entry<Date, Double> eldest) {
+			Date firstKey = cache.keySet().iterator().next();
+			if (size() > CACHE_SIZE) {
+				dailyBalanceCache.put(firstKey, cache.get(firstKey));
+				sort(dailyBalanceCache, true);
+				cache.remove(firstKey);
+			}
+			return false;
+		}
+	};
+
+	private final Map<Date, Double> dailyBalanceCache = new LinkedHashMap<Date, Double>(CACHE_SIZE,
+			(float) 0.75, true) {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		protected synchronized boolean removeEldestEntry(Map.Entry<Date, Double> eldest) {
+			Date firstKey = dailyBalanceCache.keySet().iterator().next();
+			// Remove the first element and print out the previous date's daily
+			// balance. The first element should be the oldest date in the
+			// cache.
+			if (size() > CACHE_SIZE) {
+				if (prevDate == null) {
+					prevDate = firstKey;
+					dailyBalance += dailyBalanceCache.get(firstKey);
+				}
+				dailyBalance += dailyBalanceCache.get(firstKey);
+				System.out.println(prevDate + ": " + dailyBalance);
+				dailyBalanceCache.remove(firstKey);
+			}
+			return false;
 		}
 	};
 	private double totalBalance = 0.0;
+	private double dailyBalance = 0.0;
+	private Date prevDate = null;
 
 	public TransactionCache() {
 		;
@@ -40,11 +75,38 @@ public class TransactionCache {
 
 	public void printRemaining() {
 		synchronized (cache) {
+			sort(cache, true);
 			for (Entry<Date, Double> entry : cache.entrySet()) {
-				System.out.println(entry.getKey() + ": " + entry.getValue());
+				Date key = entry.getKey();
+				if (prevDate == null) {
+					prevDate = key;
+					dailyBalance += entry.getValue();
+				}
+				dailyBalance += entry.getValue();
+				System.out.println(prevDate + ": " + dailyBalance);
+				prevDate = key;
+				// System.out.println(key + ": " + entry.getValue());
 			}
 		}
 		System.out.println("Total: " + totalBalance);
+	}
+
+	public synchronized void sort(Map<Date, Double> map, boolean ascending) {
+		List<Map.Entry<Date, Double>> entries = new ArrayList<Map.Entry<Date, Double>>(
+				map.entrySet());
+
+		if (ascending) {
+			Collections.sort(entries, (Entry<Date, Double> a, Entry<Date, Double> b) -> a.getKey()
+					.compareTo(b.getKey()));
+		} else {
+			Collections.sort(entries, Collections.reverseOrder((Entry<Date, Double> a,
+					Entry<Date, Double> b) -> a.getKey().compareTo(b.getKey())));
+		}
+
+		map.clear();
+		for (Map.Entry<Date, Double> e : entries) {
+			map.put(e.getKey(), e.getValue());
+		}
 	}
 
 	public synchronized void updateTotalBalance(double update) {
